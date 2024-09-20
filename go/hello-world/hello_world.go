062ba482a34f3012e7c1a33c37c1f6e5e5fa9b5d
veretennikovalexey <raidex@yandex.ru>
2024-09-20 15:06:51 +0300

    alex

---
+++ go/hello-world/hello_world.go
@@ -0,0 +1,16 @@
+package greeting
+
+func HelloWorld() string {
+  return "Hello, World!"
+}
+
+// я загрузил программу для установки Экзерцизма локально с гитхаба 
+// github.com/exercism
+// запустил конфигуратор с помощью токена
+// exercism configure --token=
+// скачал задание
+// exercism download --track=go --exercise=hello-world
+// запустил тесты
+// go test
+// и, наконец, выгрузил обратно свой результат
+// exercism submit hello_world.go
